3568b86d67fa675943136d2e0328fd84a2c1c2da
RashidLadj <RashidLadj@gmail.com>
2021-08-31 17:27:00 +0200

    The Resistance : Step 4 using Python

--- Training/Classic Puzzle - Very Hard/The_Resistance.py
+++ Training/Classic Puzzle - Very Hard/The_Resistance.py
@@ -25,7 +25,7 @@ def toMorse (word):
     return morseWord_
 
 
-def solve(sequence, start):
+def solve(sequence, start, resultTemp):
     """
       recursive function which calculates the number of sentences 
       that can be decoded from a sequence of Morse code.
@@ -41,22 +41,45 @@ def solve(sequence, start):
 
     if (start == len(sequence)):
         return 1
+    
+    # if resultTemp[start] != math.nan:
+    if not math.isnan(resultTemp[start]):
+
+        return resultTemp[start]
+
 
     for morseWord in morseWords:
         if morseWord == sequence[start: start + len(morseWord)]: 
-            counter += solve(sequence , start + len(morseWord))
-    
+            occurence = morseWords[morseWord]
+            counter += occurence * solve(sequence , start + len(morseWord), resultTemp)
+
+    resultTemp[start] = counter
+
     return counter
 
 
 ##########  Read Data ##########
-morseWords = [] # list of words in the dictionary coded in Morse code
+morseWords = {} # list of words in the dictionary coded in Morse code
+resultTemp = [] # list of results for each subsequence start in index i not to calculate each time
 
 morseSequence = input()
 dictionarySize = int(input())
 
 for i in range(dictionarySize):
-    morseWords.append( toMorse(input())) # get word, convert it to morse langage, and add it to out list
+    morseWord = toMorse(input())   # get word and convert it to morse langage
+
+    # /** This is for optimization in solve method **/
+    # /** if x different word have same morse code, we just give value "x" to the key "morseWord" **/
+    # /** the goal is to avoid looping multiple times on equivalent words when we can just do a simple multiplication **/
+    #
+    if morseWord in morseWords:
+        morseWords[morseWord] += 1
+    else:
+        morseWords[morseWord] = 1
 
 ##########  solve problem ##########
-print (solve (morseSequence, 0))
+for i in range(len(morseSequence)):
+    resultTemp.append(math.nan)
+
+print (solve (morseSequence, 0, resultTemp))
+
