1ef17eb374bbf972424a4885940ee121b02b1232
RashidLadj <RashidLadj@gmail.com>
2021-09-14 15:19:14 +0200

    Sudoku_Validator : All steps using Python

---
+++ Training/Classic Puzzle - Easy/Sudoku_Validator.py
@@ -0,0 +1,34 @@
+import numpy as np
+import itertools
+
+##############################################
+def solve(sudokuGrid):
+    
+    perfect = np.asarray([1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=np.uint8)
+    for i in range (9):
+        test = np.sort(sudokuGrid[i, :], axis=None)
+        if not np.array_equal(test, perfect):
+            return False
+    
+    for j in range (9):
+        test = np.sort(sudokuGrid[:, j], axis=None)
+        if not np.array_equal(test, perfect):
+            return False
+
+    for i, j in itertools.product(range(3), range(3)):
+        test = np.sort(sudokuGrid[i * 3: 3 * (i+1), j * 3 : 3 * (j+1)], axis=None)
+        if not np.array_equal(test, perfect):
+            return False
+
+    return True
+
+##############################################
+mySudoku = []
+
+for i in range(9):
+    for j in input().split():
+        mySudoku = np.append(mySudoku, int(j))
+
+mySudoku = mySudoku.reshape((9, 9))
+
+print("true") if solve(mySudoku) else print("false")
